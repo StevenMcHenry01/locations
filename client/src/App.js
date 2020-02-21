42df00361cbd38928f115ee9b833a0c751b266ec
steven mchenry <stevenmchenry@stevens-mbp.attlocal.net>
2020-02-20 16:06:57 -0800

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
 // 3rd party imports
-import React, { useState } from 'react'
+import React, { useState, Suspense } from 'react'
 import GlobalStyles from './styles/global'
 import theme from 'styled-theming'
 import styled, { ThemeProvider } from 'styled-components'
 import { useRoutes } from 'hookrouter'
 
 // My imports
-import Users from './pages/Users'
-import NewPlace from './pages/NewPlace'
-import NotFoundPage from './pages/NotFoundPage'
-import UserPlaces from './pages/UserPlaces'
+// import Users from './pages/Users'
+// import NewPlace from './pages/NewPlace'
+// import NotFoundPage from './pages/NotFoundPage'
+// import UserPlaces from './pages/UserPlaces'
+// import UpdatePlace from './pages/UpdatePlace'
+// import Auth from './pages/Auth'
 import MainNavigation from './components/Shared/Navigation/MainNavigation'
 import { Theme } from './styles/theme'
-import UpdatePlace from './pages/UpdatePlace'
-import Auth from './pages/Auth'
 import { AuthContext } from './context/auth-context'
 import { useAuth } from './hooks/auth-hook'
+import LoadingSpinner from './components/Shared/UIElements/LoadingSpinner'
 
-// ROUTES
+// ~ Code splitting
+const Users = React.lazy(() => import('./pages/Users'))
+const NewPlace = React.lazy(() => import('./pages/NewPlace'))
+const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'))
+const UserPlaces = React.lazy(() => import('./pages/UserPlaces'))
+const UpdatePlace = React.lazy(() => import('./pages/UpdatePlace'))
+const Auth = React.lazy(() => import('./pages/Auth'))
+
+// ~ ROUTES
 const loggedInRoutes = {
   '/': () => <Users />,
   '/places/new': () => <NewPlace />,
@@ -61,23 +70,31 @@ export default function App() {
             lightTheme={lightTheme}
             changeThemeClickHandler={changeThemeClickHandler}
           />
-          {lightTheme ? (
-            <ThemeProvider theme={{ mode: 'light' }}>
-              {token ? (
-                <PageWrapper>{routeResult || <NotFoundPage />}</PageWrapper>
-              ) : (
-                <PageWrapper>{routeResult || <Auth />}</PageWrapper>
-              )}
-            </ThemeProvider>
-          ) : (
-            <ThemeProvider theme={{ mode: 'dark' }}>
-              {token ? (
-                <PageWrapper>{routeResult || <NotFoundPage />}</PageWrapper>
-              ) : (
-                <PageWrapper>{routeResult || <Auth />}</PageWrapper>
-              )}
-            </ThemeProvider>
-          )}
+          <Suspense
+            fallback={
+              <div className='center'>
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {lightTheme ? (
+              <ThemeProvider theme={{ mode: 'light' }}>
+                {token ? (
+                  <PageWrapper>{routeResult || <NotFoundPage />}</PageWrapper>
+                ) : (
+                  <PageWrapper>{routeResult || <Auth />}</PageWrapper>
+                )}
+              </ThemeProvider>
+            ) : (
+              <ThemeProvider theme={{ mode: 'dark' }}>
+                {token ? (
+                  <PageWrapper>{routeResult || <NotFoundPage />}</PageWrapper>
+                ) : (
+                  <PageWrapper>{routeResult || <Auth />}</PageWrapper>
+                )}
+              </ThemeProvider>
+            )}
+          </Suspense>
         </AppWrapper>
       </AuthContext.Provider>
     </ThemeProvider>
